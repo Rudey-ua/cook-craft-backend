24b067e304092fc2d98681be8f502d13b6048bbc
Maksym-Kostenko-Leasoft <maxim.kostenko@leasoft.org>
2024-03-03 21:52:15 +0200

    Fix issue with logout

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthorizationController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
-    Route::post('/logout', 'logout');
+    Route::post('logout', 'logout')->middleware('auth:sanctum');
 });
 
 Route::group(['middleware' => ['auth:sanctum']],  function() {
