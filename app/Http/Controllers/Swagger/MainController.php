b20b14d91f4ee2c2c2fd1614f0c6922d5564230c
Maksym-Kostenko-Leasoft <maxim.kostenko@leasoft.org>
2024-03-10 15:35:00 +0200

    Add swagger documentation

---
+++ app/Http/Controllers/Swagger/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+/**
+ * @OA\Info(
+ *     title="Zemfyra API",
+ *     version="1.0.0"
+ * ),
+ * @OA\PathItem(
+ *     path="/api/"
+ * )
+ * @OA\SecurityScheme(
+ *       type="http",
+ *       description="Laravel Sanctum Bearer Token",
+ *       name="Bearer",
+ *       in="header",
+ *       scheme="bearer",
+ *       bearerFormat="JWT",
+ *       securityScheme="bearerAuth"
+ *   )
+ */
+class MainController
+{
+
+}
--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(AuthorizationController::class)->group(function () {
-    Route::post('/register', 'register');
-    Route::post('/login', 'login');
-    Route::post('logout', 'logout')->middleware('auth:sanctum');
+    Route::post('/auth/register', 'register');
+    Route::post('/auth/login', 'login');
+    Route::post('/auth/logout', 'logout')->middleware('auth:sanctum');
 });
 
 Route::group(['middleware' => ['auth:sanctum']],  function() {
