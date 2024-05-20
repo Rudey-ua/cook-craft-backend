d9f35e18a0048143108631adb624826e26939776
Maksym-Kostenko-Leasoft <maxim.kostenko@leasoft.org>
2024-05-20 22:48:26 +0300

    Profile update

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateProfileRequest;
 use App\Http\Resources\UserResource;
 use App\Repositories\SubscriptionRepository;
+use App\Services\UserService;
 use F9Web\ApiResponseHelpers;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     use ApiResponseHelpers;
 
-    public function __construct(protected SubscriptionRepository $subscriptionRepository)
+    public function __construct(protected SubscriptionRepository $subscriptionRepository, protected UserService $userService)
     {
     }
 
@@ -23,5 +24,16 @@ class UserController extends Controller
             'message' => 'Data successfully retrieved!',
             'data' => new UserResource(Auth::user())
         ]);
+    }
+
+    public function updateProfile(UpdateProfileRequest $request)
+    {
+        $validated = $request->validated();
+
+        try {
+            return $this->userService->updateProfileData($validated, Auth::user());
+        } catch (\Exception $e) {
+            return $this->respondError($e->getMessage());
+        }
     }
 }
---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstName' => 'nullable|string|min:3',
+            'lastName' => 'nullable|string|min:3',
+            'email' => 'nullable|email|unique:users',
+            'password' => ['nullable', Password::min(8)->letters()->numbers()],
+            'birthDate' => 'nullable|date|before_or_equal:today',
+            'gender' => 'nullable|in:male,female',
+            'language' => 'nullable',
+            'time_zone' => 'nullable'
+        ];
+    }
+}
