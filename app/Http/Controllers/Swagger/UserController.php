3c2aaeb47a5753f3366788298f5bbc5992a25c0d
Maksym-Kostenko-Leasoft <maxim.kostenko@leasoft.org>
2024-05-25 15:47:11 +0300

    Added docs

--- app/Http/Controllers/Swagger/UserController.php
+++ app/Http/Controllers/Swagger/UserController.php
@@ -149,6 +149,80 @@ namespace App\Http\Controllers\Swagger;
  *           )
  *       )
  *  )
+ * @OA\Post(
+ *       path="/api/users/update",
+ *       summary="Update user profile",
+ *       tags={"Profile"},
+ *       @OA\RequestBody(
+ *           required=true,
+ *           @OA\MediaType(
+ *               mediaType="multipart/form-data",
+ *               @OA\Schema(
+ *                   type="object",
+ *                   @OA\Property(property="firstName", type="string", description="nullable|string|min:3"),
+ *                   @OA\Property(property="lastName", type="string", description="nullable|string|min:3"),
+ *                   @OA\Property(property="email", type="string", format="email", description="nullable|email|unique:users"),
+ *                   @OA\Property(property="password", type="string", description="nullable|Password with at least 8 characters including letters and numbers"),
+ *                   @OA\Property(property="birthDate", type="string", format="date", description="nullable|date|before_or_equal:today"),
+ *                   @OA\Property(property="profileImage", type="string", format="binary", description="nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048"),
+ *                   @OA\Property(property="gender", type="string", enum={"male", "female"}, description="nullable|in:male,female"),
+ *                   @OA\Property(property="language", type="string", description="nullable"),
+ *                   @OA\Property(property="timezone", type="string", description="nullable")
+ *               )
+ *           )
+ *       ),
+ *       @OA\Response(
+ *           response=200,
+ *           description="User profile updated successfully",
+ *           @OA\JsonContent(
+ *               type="object",
+ *               @OA\Property(property="message", type="string", example="User profile updated successfully"),
+ *               @OA\Property(
+ *                   property="data", type="object",
+ *                   @OA\Property(property="id", type="integer", example=3),
+ *                   @OA\Property(property="firstname", type="string", example="Den"),
+ *                   @OA\Property(property="lastname", type="string", example="Kostenko"),
+ *                   @OA\Property(property="email", type="string", format="email", example="koctenko525@gmail.com"),
+ *                   @OA\Property(property="profile_image", type="string", example="https://canteen-app.pp.ua/storage/profile_images/6651dcf7ab7d02.74289847_23dd31f39f75c3852fd6c1f6a9d915c3662a3f7689970.jpeg"),
+ *                   @OA\Property(property="is_active", type="boolean", example=true),
+ *                   @OA\Property(
+ *                       property="userDetails", type="object",
+ *                       @OA\Property(property="birth_date", type="string", format="date", example="2002-11-10"),
+ *                       @OA\Property(property="gender", type="string", example="male"),
+ *                       @OA\Property(property="language", type="string", example="ru"),
+ *                       @OA\Property(property="time_zone", type="string", example="Europe/Kyiv")
+ *                   ),
+ *                   @OA\Property(
+ *                       property="role", type="array",
+ *                       @OA\Items(type="object",
+ *                           @OA\Property(property="id", type="integer", example=3),
+ *                           @OA\Property(property="name", type="string", example="member"),
+ *                           @OA\Property(property="guard_name", type="string", example="web"),
+ *                           @OA\Property(property="created_at", type="string", format="date-time", example="2024-03-03T20:08:04.000000Z"),
+ *                           @OA\Property(property="updated_at", type="string", format="date-time", example="2024-03-03T20:08:04.000000Z"),
+ *                           @OA\Property(
+ *                               property="pivot", type="object",
+ *                               @OA\Property(property="model_type", type="string", example="App\\Models\\User"),
+ *                               @OA\Property(property="model_id", type="integer", example=3),
+ *                               @OA\Property(property="role_id", type="integer", example=3)
+ *                           )
+ *                       )
+ *                   ),
+ *                   @OA\Property(property="is_active_subscription", type="boolean", example=false),
+ *                   @OA\Property(property="is_canceled_subscription", type="boolean", example=false)
+ *               )
+ *           )
+ *       ),
+ *       @OA\Response(
+ *           response=400,
+ *           description="Bad Request - Incorrect Data Provided",
+ *           @OA\JsonContent(
+ *               type="object",
+ *               @OA\Property(property="error", type="string", example="Invalid data provided")
+ *           )
+ *       ),
+ *       security={{"bearerAuth":{}}}
+ *  )
  */
 class UserController
 {
