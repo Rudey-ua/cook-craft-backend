6d4fa78f33f047b706940602bdec7be91ccea7f7
Maksym-Kostenko-Leasoft <maxim.kostenko@leasoft.org>
2024-05-26 12:14:14 +0300

    Added docs

--- app/Http/Controllers/Swagger/RecipeController.php
+++ app/Http/Controllers/Swagger/RecipeController.php
@@ -110,6 +110,65 @@ namespace App\Http\Controllers\Swagger;
  *       ),
  *       security={{"bearerAuth":{}}}
  * )
+ * @OA\Get(
+ *       path="/api/users/recipe",
+ *       summary="Retrieve list of recipes for specific user",
+ *       tags={"Recipes"},
+ *       @OA\Response(
+ *           response=200,
+ *           description="List of recipes retrieved successfully",
+ *           @OA\JsonContent(
+ *               type="array",
+ *               @OA\Items(
+ *                   type="object",
+ *                   @OA\Property(property="id", type="integer", example=25),
+ *                   @OA\Property(property="user_id", type="integer", example=14),
+ *                   @OA\Property(property="title", type="string", example="Chimichurri Sauce"),
+ *                   @OA\Property(property="description", type="string", example="This famous Argentinian chimichurri sauce is perfect for any grilled chicken, meat, or fish. My catering customers love it on garlic crostini with grilled flank steak slices."),
+ *                   @OA\Property(property="cooking_time", type="integer", example=120),
+ *                   @OA\Property(property="difficulty_level", type="string", enum={"easy", "medium", "hard"}, example="medium"),
+ *                   @OA\Property(property="portions", type="integer", example=4),
+ *                   @OA\Property(property="is_approved", type="boolean", example=true),
+ *                   @OA\Property(property="is_published", type="boolean", example=true),
+ *                   @OA\Property(property="cover_photo", type="string", example="localhost/storage/recipe_cover_photos/66524ee229a0d3.71372581_vegetarian-recipes-1.jpg"),
+ *                   @OA\Property(
+ *                       property="ingredients",
+ *                       type="array",
+ *                       @OA\Items(
+ *                           type="object",
+ *                           @OA\Property(property="id", type="integer", example=51),
+ *                           @OA\Property(property="title", type="string", example="fresh parsley"),
+ *                           @OA\Property(property="measure", type="string", example="cup"),
+ *                           @OA\Property(property="count", type="integer", example=1)
+ *                       )
+ *                   ),
+ *                   @OA\Property(
+ *                       property="steps",
+ *                       type="array",
+ *                       @OA\Items(
+ *                           type="object",
+ *                           @OA\Property(property="id", type="integer", example=42),
+ *                           @OA\Property(property="description", type="string", example="Combine parsley, oil, vinegar, oregano, cumin, garlic, hot sauce, and salt in a blender or food processor. Mix on medium speed until ingredients are evenly blended, about 10 seconds."),
+ *                           @OA\Property(
+ *                               property="photos",
+ *                               type="array",
+ *                               @OA\Items(type="string", example="localhost/storage/recipe_step_photos/66524ee22f2225.89812296_k_Photo_Recipe_Ramp_Up_2022-02-Air-Fryer-Roasted-Potatoes_IMG_6850.jpeg")
+ *                           )
+ *                       )
+ *                   )
+ *               )
+ *           )
+ *       ),
+ *       @OA\Response(
+ *           response=400,
+ *           description="Bad request",
+ *           @OA\JsonContent(
+ *               type="object",
+ *               @OA\Property(property="error", type="string", example="Invalid request parameters")
+ *           )
+ *       ),
+ *       security={{"bearerAuth":{}}}
+ *  )
  */
 class RecipeController
 {
