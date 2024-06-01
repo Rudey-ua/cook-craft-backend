5d0f8cc8be6fe15a3dad3b0170316113ce4af398
Maksym-Kostenko-Leasoft <maxim.kostenko@leasoft.org>
2024-06-02 00:04:18 +0300

    Fix

--- app/Http/Requests/RecipeRequest.php
+++ app/Http/Requests/RecipeRequest.php
@@ -27,7 +27,7 @@ class RecipeRequest extends FormRequest
             'cooking_time' => 'required|integer|min:1',
             'difficulty_level' => 'required|in:easy,medium,hard',
             'portions' => 'required|integer|min:1',
-            'cover_photo' => 'required|file|max:2048',
+            'cover_photo' => 'required|file|max:12064',
             'ingredients' => 'required|array|min:1',
             'ingredients.*.title' => 'required|string|max:255',
             'ingredients.*.measure' => 'required|string|max:100',
@@ -35,7 +35,7 @@ class RecipeRequest extends FormRequest
             'steps' => 'required|array|min:1',
             'steps.*.description' => 'required|string|max:1000',
             'steps.*.photos' => 'array|nullable',
-            'steps.*.photos.*' => 'file|mimes:jpg,jpeg,png|max:2048',
+            'steps.*.photos.*' => 'file|mimes:jpg,jpeg,png|max:12064',
             'tags.*' => 'nullable|exists:tags,id'
         ];
     }
